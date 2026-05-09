198c8a9108aa4587efd9f738c92551589cbc5e62
Andres Montero <andresmontero@MacBook-Pro-de-Juan.local>
2026-05-09 17:13:01 -0300

    fix: sheet activar regalo dentro del JSX

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -254,7 +254,6 @@ export default function Dashboard() {
         </div>
       )}
 
-    </div>
       {suggestedToActivate && (
         <ActivarRegaloSheet
           suggested={suggestedToActivate}
